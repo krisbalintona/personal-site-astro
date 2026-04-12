84cd872ab09ca08d2136fce1b91dd91c53675933
Kristoffer Balintona <krisbalintona@gmail.com>
2026-04-12 04:43:16 -0500

    chore: Activate Eglot in .astro files

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((astro-mode . ((eval . (eglot-ensure))))
+ (astro-ts-mode . ((eval . (eglot-ensure)))))
