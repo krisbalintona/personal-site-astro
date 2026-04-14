344cdc9f15304baac7eeba11aea0e55a5bb0221a
Kristoffer Balintona <krisbalintona@gmail.com>
2026-04-14 02:22:22 -0500

    chore: Set starter MDX options

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
-import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kristofferbalintona.me",
   prerenderConflictBehavior: "error",
-  integrations: [mdx()],
+  integrations: [
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: { theme: "github-light" },
+      gfm: false,
+    }),
+  ],
 });
