7b5422606b2156d1ca40e2b13f851ef5c1c10f55
Kristoffer Balintona <krisbalintona@gmail.com>
2026-04-18 17:54:11 -0500

    build(deps): Update Guix dependencies for new pixel-llimit-fix.sh

--- manifest.scm
+++ manifest.scm
@@ -2,4 +2,8 @@
 ;; You can store it in a file that you may then pass to any 'guix' command
 ;; that accepts a '--manifest' (or '-m') option.
 
-(specifications->manifest (list "node"))
+(specifications->manifest (list "node"
+                                ;; For scripts/pixel-limit-fix.sh
+                                "ffmpeg" ; Installs ffprobe
+                                "gawk"   ; For awk
+                                "gifsicle"))
