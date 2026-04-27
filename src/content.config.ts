fdb8368b101a9feafcc2da9c4fe933c04515dd15
Kristoffer Balintona <krisbalintona@gmail.com>
2026-04-26 23:09:26 -0500

    fix(tags): No need to generate custom entry IDs

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,6 @@ import { type CollectionEntry, defineCollection } from "astro:content";
 import { postSchema } from "@lib/posts.ts";
 import { glob } from "astro/loaders";
 import { z } from "astro/zod";
-import slugify from "slugify";
 
 const articles = defineCollection({
   loader: glob({
@@ -16,8 +15,6 @@ const tags = defineCollection({
   loader: glob({
     pattern: "*/index.mdx",
     base: "./src/content/tags",
-    generateId: ({ entry, data }) =>
-      data?.name ? slugify(data.name as string) : entry.split("/")[0],
   }),
   schema: z.object({
     name: z.string(),
