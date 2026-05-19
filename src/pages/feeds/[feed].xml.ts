624dc4865e7f83ef69d8f6feb7dd48a8876d2440
Kristoffer Balintona <krisbalintona@gmail.com>
2026-05-19 02:21:09 -0500

    refactor(rss): Create page dedicated to listing RSS feeds

--- src/pages/feeds/[feed].xml.ts
+++ src/pages/feeds/[feed].xml.ts
@@ -1,5 +1,10 @@
 import { makeRSSFeed, type RSSFeed, RSSFeeds } from "@lib/rss.ts";
 import type { APIContext } from "astro";
+import { createRoute } from "astro-typesafe-routes/create-route";
+
+export const Route = createRoute({
+  routeId: "/feeds/[feed].xml",
+});
 
 export function getStaticPaths() {
   return Object.entries(RSSFeeds).map(([feedName, channel]) => ({
